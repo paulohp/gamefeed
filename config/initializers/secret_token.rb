067a77416b51837d20e4f3c7faf96a8c4e8ff4f0
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-08-10 11:25:18 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gamefeed::Application.config.secret_key_base = 'df012f6968f3aaa8e3bb712e6f52ba9c582bbc103b3a4f7a31fd354b3a48bc3817b4bfb9af9de874cea82391e6910a298661a4fc602ff61b7b7922e34935b7e2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gamefeed::Application.config.session_store :cookie_store, key: '_gamefeed_session'
